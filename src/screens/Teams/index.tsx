c938d679aa2d889c0fdffbb4fbfd2eeb6ce2b01e
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-10-06 15:33:34 -0300

    feat(components): create `Highlight`

--- src/screens/Teams/index.tsx
+++ src/screens/Teams/index.tsx
@@ -1,11 +1,16 @@
 import { Container } from "./styles";
 
 import Header from "@components/Header";
+import Highlight from "@components/Highlight";
 
 export default function Teams() {
     return (
         <Container>
             <Header showBackButton />
+            <Highlight
+                title="Teams"
+                subtitle="Play with your team"
+            />
         </Container>
     );
 }
