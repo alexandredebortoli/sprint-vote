b62b2ac722830395766cde0dd0b0fcef6558c4c3
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-10-05 21:43:30 -0300

    feat(theme): add theme map

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components";
+
+import theme from "@theme/index";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <View style={styles.container}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+                <StatusBar style="auto" />
+            </View>
+        </ThemeProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
