58bb3317695621ff5e28e285d9b497eb9795d0cd
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-10-06 19:53:08 -0300

    feat(screens/Team): create filter bar

--- src/screens/Team/index.tsx
+++ src/screens/Team/index.tsx
@@ -1,10 +1,19 @@
 import Header from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfItems } from "./styles";
 import Highlight from "@components/Highlight";
 import ButtonIcon from "@components/ButtonIcon";
 import Input from "@components/Input";
+import { FlatList } from "react-native";
+import Filter from "@components/Filter";
+import { useState } from "react";
 
 export default function Team() {
+    const tabs = ["members", "game history"];
+
+    const [tab, setTab] = useState<string>(tabs[0]);
+    const [members, setMembers] = useState<string[]>([]);
+    const [gameHistories, setGameHistories] = useState<string[]>([]);
+
     return (
         <Container>
             <Header showBackButton />
@@ -22,6 +31,24 @@ export default function Team() {
                 />
                 <ButtonIcon icon="add" />
             </Form>
+
+            <HeaderList>
+                <FlatList
+                    data={tabs}
+                    keyExtractor={(item) => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === tab}
+                            onPress={() => setTab(item)}
+                        />
+                    )}
+                    horizontal
+                />
+                <NumberOfItems>
+                    {tab === tabs[0] ? members.length : gameHistories.length}
+                </NumberOfItems>
+            </HeaderList>
         </Container>
     );
 }
