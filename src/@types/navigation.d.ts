123243dd93bed5536ee64d004d5b1ef680e8e42e
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-10-07 14:17:29 -0300

    feat: add routes mapping

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            signIn: undefined;
+            signUp: undefined;
+            teams: undefined;
+            newTeam: undefined;
+            team: undefined;
+        }
+    }
+}
