0d6ea81e604e7442a6a32017326173710dd8674a
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-10-07 14:18:33 -0300

    feat: add app navigation

--- src/screens/Teams/index.tsx
+++ src/screens/Teams/index.tsx
@@ -8,13 +8,24 @@ import Highlight from "@components/Highlight";
 import TeamCard from "@components/TeamCard";
 import EmptyList from "@components/EmptyList";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Teams() {
     const [teams, setTeams] = useState<string[]>(["Team 1", "Team 2"]);
 
+    const navigation = useNavigation();
+
+    function handleNewTeam() {
+        navigation.navigate("newTeam");
+    }
+
+    function handleOpenTeam() {
+        navigation.navigate("team");
+    }
+
     return (
         <Container>
-            <Header showBackButton />
+            <Header />
 
             <Highlight
                 title="Teams"
@@ -24,14 +35,22 @@ export default function Teams() {
             <FlatList
                 data={teams}
                 keyExtractor={(item) => item}
-                renderItem={({ item }) => <TeamCard title={item} />}
+                renderItem={({ item }) => (
+                    <TeamCard
+                        title={item}
+                        onPress={handleOpenTeam}
+                    />
+                )}
                 contentContainerStyle={teams.length === 0 && { flex: 1 }}
                 ListEmptyComponent={() => (
                     <EmptyList message="👀 What about creating your first team?" />
                 )}
             />
 
-            <Button title="Create new team" />
+            <Button
+                title="Create new team"
+                onPress={handleNewTeam}
+            />
         </Container>
     );
 }
