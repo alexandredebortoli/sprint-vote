5958b418f9dc9933b37f844a3ca74e1d84eaf2cf
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-10-12 11:33:44 -0300

    feat: persist auth token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,18 @@
-import { ReactNode, createContext, useState } from "react";
-
-import { UserAuthDTO } from "@dtos/user-auth.dto";
+import { ReactNode, createContext, useEffect, useState } from "react";
 
 import { api } from "@services/api";
 
+import {
+    storageAuthTokenSave,
+    storageAuthTokenRemove,
+    storageAuthTokenGet,
+} from "@storage/storageAuthToken";
+
 export type AuthContextDataProps = {
-    user: UserAuthDTO;
+    token: string;
     signIn: (email: string, password: string) => Promise<void>;
+    signOut: () => Promise<void>;
+    isLoadingUserStorageData: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -18,25 +24,73 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-    const [user, setUser] = useState<UserAuthDTO>({} as UserAuthDTO);
+    const [token, setToken] = useState<string>("");
+    const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
+        useState<boolean>(true);
+
+    async function tokenUpdate(accessToken: string) {
+        api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+        setToken(accessToken);
+    }
 
     async function signIn(email: string, password: string) {
         try {
-            const { data: user } = await api.post("/sessions", {
+            const { data } = await api.post("/sessions", {
                 email,
                 password,
             });
 
-            if (user.access_token) {
-                setUser(user);
+            if (data.access_token) {
+                setIsLoadingUserStorageData(true);
+
+                await storageAuthTokenSave(data.access_token);
+
+                tokenUpdate(data.access_token);
             }
         } catch (error) {
             throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
         }
     }
 
+    async function signOut() {
+        try {
+            setIsLoadingUserStorageData(true);
+
+            setToken("");
+            await storageAuthTokenRemove();
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    async function loadAuthTokenData() {
+        try {
+            setIsLoadingUserStorageData(true);
+
+            const accessToken = await storageAuthTokenGet();
+
+            if (!!accessToken) {
+                tokenUpdate(accessToken);
+            }
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    useEffect(() => {
+        loadAuthTokenData();
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ user, signIn }}>
+        <AuthContext.Provider
+            value={{ token, signIn, signOut, isLoadingUserStorageData }}
+        >
             {children}
         </AuthContext.Provider>
     );
