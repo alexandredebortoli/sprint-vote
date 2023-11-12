b0d00c0dc8bb0c025d5c653f211bbdf020540ef3
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2023-11-12 19:21:20 -0300

    fix: unit test remove => not found exception

--- server/src/teams/teams.service.spec.ts
+++ server/src/teams/teams.service.spec.ts
@@ -128,7 +128,7 @@ describe('TeamsService', () => {
       jest.spyOn(mockTeamRepository, 'findOne').mockResolvedValue(null);
 
       try {
-        await service.findOne('1');
+        await service.remove('1');
       } catch (error) {
         expect(error).toBeInstanceOf(NotFoundException);
         if (error instanceof NotFoundException) {
